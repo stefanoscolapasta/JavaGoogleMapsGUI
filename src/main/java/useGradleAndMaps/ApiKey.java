207cb05ae2c34ea15fa630f2218f56fb1851de1d
StefanoScolari <stefano.scolari@studio.unibo.it>
2020-11-22 10:52:53 +0100

    changed some error handling

--- src/main/java/useGradleAndMaps/ApiKey.java
+++ src/main/java/useGradleAndMaps/ApiKey.java
@@ -17,12 +17,14 @@ public class ApiKey {
                                             + "EclipseWorkSpace\\useGradleAndMaps\\"
                                             + "ApiKey\\key.txt";
     
-    public ApiKey() throws UnsupportedEncodingException, FileNotFoundException, IOException {
+    public ApiKey() throws IOException {
         try(final BufferedReader r = new BufferedReader(
                 new InputStreamReader(
                         new FileInputStream(ApiKey.FILE_NAME), "UTF-8"))){
             this.key = r.readLine();
-        };
+        } catch(IOException exc) {
+            System.out.println(exc.getMessage());
+        }
     }
     
     public String getApiKey() {
